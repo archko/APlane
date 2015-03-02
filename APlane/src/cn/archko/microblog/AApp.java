4c6b521af705ced7383a0ee721882485eb156294
archko <archko@gmail.com>
2015-03-02 13:17:43 +0800

    reset max image height remove intent-filter

--- APlane/src/cn/archko/microblog/AApp.java
+++ APlane/src/cn/archko/microblog/AApp.java
@@ -18,7 +18,7 @@ public class AApp extends App {
         AppSettings.init();
         ImageFetcher.getInstance(this).initImageOption(DisplayUtils.getScreenWidth()*2, DisplayUtils.getScreenHeight()*2, Bitmap.Config.RGB_565);
         Canvas canvas=new Canvas();
-        if (canvas.isHardwareAccelerated()) {
+        if (canvas.getMaximumBitmapHeight()<=4096) {
             ImageFetcher.DEFAULT_MAX_IMAGE_TEXTURE_HEIGHT=canvas.getMaximumBitmapHeight();
             ImageFetcher.DEFAULT_MAX_IMAGE_TEXTURE_WIDTH=canvas.getMaximumBitmapWidth();
         }
